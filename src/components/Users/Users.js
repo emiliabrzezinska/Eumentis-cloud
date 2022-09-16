ecf4f4e24979fc77a356aff2f1a103307317ddb7
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2022-09-16 23:18:16 +0200

    rendering all users with data

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,66 +4,77 @@ import "./Users.css";
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [avatars, setAvatars] = useState([]);
-  const [isPending, setIsPending] = useState(true)
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    setTimeout (()=>{   
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setUsers(data); 
-        setIsPending(false);
-      });
-      }, 1000);
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setUsers(data);
+          setIsPending(false);
+          console.log(data);
+        });
+    }, 1000);
   }, []);
 
   useEffect(() => {
-    setTimeout (()=>{    
-      fetch("https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy"
-    )
-      .then((response) => {
-        console.log(response)
-        return response.url();
-      })
-      .then((data) => {
-        setAvatars(data); 
-        setIsPending(false);
-      }); 
-      }, 1000);
-  },[]);
+    setTimeout(() => {
+      fetch(
+        "https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy"
+      )
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          setAvatars(data);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div>
-      {isPending && <div className="sk-circle">
-  <div className="sk-circle1 sk-child"></div>
-  <div className="sk-circle2 sk-child"></div>
-  <div className="sk-circle3 sk-child"></div>
-  <div className="sk-circle4 sk-child"></div>
-  <div className="sk-circle5 sk-child"></div>
-  <div className="sk-circle6 sk-child"></div>
-  <div className="sk-circle7 sk-child"></div>
-  <div className="sk-circle8 sk-child"></div>
-  <div className="sk-circle9 sk-child"></div>
-  <div className="sk-circle10 sk-child"></div>
-  <div className="sk-circle11 sk-child"></div>
-  <div className="sk-circle12 sk-child"></div>
-</div>}
+      {isPending && (
+        <div className="sk-circle">
+          <div className="sk-circle1 sk-child"></div>
+          <div className="sk-circle2 sk-child"></div>
+          <div className="sk-circle3 sk-child"></div>
+          <div className="sk-circle4 sk-child"></div>
+          <div className="sk-circle5 sk-child"></div>
+          <div className="sk-circle6 sk-child"></div>
+          <div className="sk-circle7 sk-child"></div>
+          <div className="sk-circle8 sk-child"></div>
+          <div className="sk-circle9 sk-child"></div>
+          <div className="sk-circle10 sk-child"></div>
+          <div className="sk-circle11 sk-child"></div>
+          <div className="sk-circle12 sk-child"></div>
+        </div>
+      )}
       <ul>
-        {users.map(({ id, name, email, city }) => (
-          <li key={id}>
+        {users.map((user) => (
+          <li key={user.id}>
             <br></br>
-            {name}
+            {user.name}
             <br></br>
-            {email}
+            Email: {user.email}
             <br></br>
-            {city}
+            Phone: {user.phone}
+            <br></br>
+            Company: {user.company.name}
+            <br></br>
+            Website: {user.website}
+            <br></br>
+            Address: {user.address.street}, {user.address.suite},
+            {user.address.city}, {user.address.zipcode}
           </li>
         ))}
       </ul>
       <ul>
-        {avatars.map(({user}) => (
+        {avatars.map(({ user }) => (
           <li key={user.id}>
             <img src={user.username}></img>
           </li>
