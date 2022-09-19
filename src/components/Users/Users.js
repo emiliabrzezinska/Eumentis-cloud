bec2e268868f4d40488ac85713df7d078c4db502
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2022-09-19 23:12:00 +0200

    moving loading indicator to a separate component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Loading from "../Loading/Loading.js";
 import "./Users.css";
 
 const Users = () => {
@@ -6,55 +7,41 @@ const Users = () => {
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          setUsers(data);
-          setIsPending(false);
-          console.log(data);
-        });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setIsPending(false);
+        console.log(data);
+      });
   }, []);
 
   return (
     <div>
-      {isPending && (
-        <div className="sk-circle">
-          <div className="sk-circle1 sk-child"></div>
-          <div className="sk-circle2 sk-child"></div>
-          <div className="sk-circle3 sk-child"></div>
-          <div className="sk-circle4 sk-child"></div>
-          <div className="sk-circle5 sk-child"></div>
-          <div className="sk-circle6 sk-child"></div>
-          <div className="sk-circle7 sk-child"></div>
-          <div className="sk-circle8 sk-child"></div>
-          <div className="sk-circle9 sk-child"></div>
-          <div className="sk-circle10 sk-child"></div>
-          <div className="sk-circle11 sk-child"></div>
-          <div className="sk-circle12 sk-child"></div>
-        </div>
-      )}
+      {isPending && <Loading />}
 
       {users.map((user) => (
         <div className="view" key={user.id}>
           <div className="avatar">
-          <img alt="person with a specyfic mood"
-            src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
-          ></img>
+            <img
+              alt="person with a specyfic mood"
+              src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+            ></img>
           </div>
           <div className="text">
-          <div className="name">{user.name}</div>
-          <span className="data">Email:</span> {user.email}
-          <br></br>
-          <span className="data">Phone:</span> {user.phone}
-          <br></br>
-          <span className="data">Company:</span> {user.company.name}
-          <br></br>
-          <span className="data">Website:</span> {user.website}
-          <br></br>
-          <span className="data">Address:</span> {user.address.street},{" "}
-          {user.address.suite},{user.address.city}, {user.address.zipcode}
+            <div className="name">{user.name}</div>
+            <span className="data">Email:</span> {user.email}
+            <br></br>
+            <span className="data">Phone:</span> {user.phone}
+            <br></br>
+            <span className="data">Company:</span> {user.company.name}
+            <br></br>
+            <span className="data">Website:</span> {user.website}
+            <br></br>
+            <span className="data">Address:</span> {user.address.street},{" "}
+            {user.address.suite},{user.address.city}, {user.address.zipcode}
           </div>
         </div>
       ))}
